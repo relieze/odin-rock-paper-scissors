18e3ad9ff77f83157a7fdda1c349e0e24bc69fa9
relieze <127432944+relieze@users.noreply.github.com>
2023-08-02 15:46:57 -0700

    Add game funct to run 5 rounds; update round funct
    Add a game function to run 5 rounds of rock paper scissors
      - if a tie, the round resets
    
    Update round funtion
      - code was a bit repetitive so simplified it

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,43 @@
-// returns choice based on random number
+// returns random choice; taken as computer's choice
 function getComputerChoice() {
   let num = Math.ceil(Math.random() * 3);
   return num == 1 ? "Rock" : num == 2 ? "Paper" : "Scissors";
 }
 
+// compares choices of player and computer to determine winner
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
+  playerSelection =
+    playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
   if (playerSelection == computerSelection) return "You Tied!";
 
-  if (playerSelection == "rock") {
-    return computerSelection == "scissors"
-      ? "You win! Rock beats Scissors!"
-      : "You Lose! Paper beats Rock!";
-  };
+  let playerWin =
+    (playerSelection == "Rock" && computerSelection == "Scissors") ||
+    (playerSelection == "Scissors" && computerSelection == "Paper") ||
+    (playerSelection == "Paper" && computerSelection == "Rock");
 
-  if (playerSelection == "scissors") {
-    return computerSelection == "paper"
-      ? "You win! Scissors beats Paper!"
-      : "You Lose! Rock beats Scissors!";
-  };
+  return playerWin
+    ? `You win! ${playerSelection} beats ${computerSelection}`
+    : `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
 
-  if (playerSelection == "paper") {
-    return computerSelection == "rock"
-      ? "You win! Paper beats Rock!"
-      : "You Lose! Scissors beats Paper!";
-  };
-};
+// run a whole game
+function game() {
+  // run five rounds, prompting user for their choice, and receiving computer's choice
+  for (round = 1; round <= 5; round++) {
+    let playerSelection = prompt("Rock, Paper, or Scissors? ");
+    let computerSelection = getComputerChoice();
 
-const playerSelection = "paper";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    // if choice are he same, round is reset
+    if (playRound(playerSelection, computerSelection) == "You Tied!") {
+      round--;
+      console.log("You tied! Go again!");
+      continue;
+    }
+    console.log(
+      `Round ${round}: ${playRound(playerSelection, computerSelection)}`
+    );
+  }
+}
+
+game();
