f668ef1a3198f1c8d93a05f6e46873a1f793149e
relieze <127432944+relieze@users.noreply.github.com>
2023-08-02 13:30:34 -0700

    Add function to play a around and get winner

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,34 @@
-// returns choice based on random number 
+// returns choice based on random number
 function getComputerChoice() {
   let num = Math.ceil(Math.random() * 3);
   return num == 1 ? "Rock" : num == 2 ? "Paper" : "Scissors";
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection == computerSelection) return "You Tied!";
+
+  if (playerSelection == "rock") {
+    return computerSelection == "scissors"
+      ? "You win! Rock beats Scissors!"
+      : "You Lose! Paper beats Rock!";
+  };
+
+  if (playerSelection == "scissors") {
+    return computerSelection == "paper"
+      ? "You win! Scissors beats Paper!"
+      : "You Lose! Rock beats Scissors!";
+  };
+
+  if (playerSelection == "paper") {
+    return computerSelection == "rock"
+      ? "You win! Paper beats Rock!"
+      : "You Lose! Scissors beats Paper!";
+  };
 };
 
-console.log(getComputerChoice());
-
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
