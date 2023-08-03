736e5dc5de7f29740b5ec06f28a58948f887d649
relieze <127432944+relieze@users.noreply.github.com>
2023-08-02 21:35:25 -0700

    Finish program, decide winner; reorganize js code

--- javascript/script.js
+++ javascript/script.js
@@ -1,43 +1,65 @@
-// returns random choice; taken as computer's choice
+// returns random number which is converted into computer's choice
 function getComputerChoice() {
   let num = Math.ceil(Math.random() * 3);
-  return num == 1 ? "Rock" : num == 2 ? "Paper" : "Scissors";
+  return num === 1 ? "Rock" : num === 2 ? "Paper" : "Scissors";
 }
 
 // compares choices of player and computer to determine winner
-function playRound(playerSelection, computerSelection) {
-  playerSelection =
-    playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+// return outcome: true(win), false(loss), or tie
+function playRound(playerSelection, computerSelection, round) {
+  if (playerSelection === computerSelection) return "tie";
 
-  if (playerSelection == computerSelection) return "You Tied!";
+  // "outcome" is true if player's choice beats computer's choice
+  // false if none of the expressions are true, player's choice loses
+  let outcome =
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock");
 
-  let playerWin =
-    (playerSelection == "Rock" && computerSelection == "Scissors") ||
-    (playerSelection == "Scissors" && computerSelection == "Paper") ||
-    (playerSelection == "Paper" && computerSelection == "Rock");
+  // print who won based on outcome's boolean value
+  console.log(
+    outcome
+      ? `Round ${round}: You win! ${playerSelection} beats ${computerSelection}.`
+      : `Round ${round}: You Lose! ${computerSelection} beats ${playerSelection}.`
+  );
 
-  return playerWin
-    ? `You win! ${playerSelection} beats ${computerSelection}`
-    : `You Lose! ${computerSelection} beats ${playerSelection}`;
+  return outcome;
 }
 
 // run a whole game
 function game() {
+  // accumlating counter; goes up everytime the user wins a round
+  let win = 0;
+
   // run five rounds, prompting user for their choice, and receiving computer's choice
-  for (round = 1; round <= 5; round++) {
+  for (let round = 1; round <= 5; round++) {
     let playerSelection = prompt("Rock, Paper, or Scissors? ");
     let computerSelection = getComputerChoice();
 
-    // if choice are he same, round is reset
-    if (playRound(playerSelection, computerSelection) == "You Tied!") {
+    // reformat user's choice for capitalization
+    playerSelection =
+      playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    // run function to determine outcome
+    const outcome = playRound(playerSelection, computerSelection, round);
+
+    // if a tie, round is repeated 
+    // if user wins the round, "win" is incremented
+    if (outcome === "tie") {
       round--;
       console.log("You tied! Go again!");
-      continue;
+    } else {
+      win += outcome;
     }
-    console.log(
-      `Round ${round}: ${playRound(playerSelection, computerSelection)}`
-    );
+
+    // ends loop if either the computer or user has the necessary 3 wins
+    if (win === 3 || win === round - 3) break;
   }
+
+  return win;
 }
 
-game();
+// prints final message based on outcome: either a win or loss
+console.log(
+  game() === 3 ? "YOU WON!!!!!" : "Awww, you lost... Better luck next time!"
+);
