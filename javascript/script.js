3d20f718312ab9f915fc26023b66445f155e3ed7
relieze <127432944+relieze@users.noreply.github.com>
2023-08-05 20:44:10 -0700

    Update commenting so not as wordy and repetitive

--- javascript/script.js
+++ javascript/script.js
@@ -9,14 +9,13 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection, round) {
   if (playerSelection === computerSelection) return "tie";
 
-  // "outcome" is true if player's choice beats computer's choice
-  // false if none of the expressions are true, player's choice loses
+  // "outcome" = true if user wins round, false otherwise
   let outcome =
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Rock");
 
-  // print who won based on outcome's boolean value
+  // print winner of round based on "outcome"
   console.log(
     outcome
       ? `Round ${round}: You win! ${playerSelection} beats ${computerSelection}.`
@@ -26,10 +25,8 @@ function playRound(playerSelection, computerSelection, round) {
   return outcome;
 }
 
-// run a whole game
 function game() {
-  // accumlating counter; goes up everytime the user wins a round
-  let win = 0;
+  let roundsWon = 0;
 
   // run five rounds, prompting user for their choice, and receiving computer's choice
   for (let round = 1; round <= 5; round++) {
@@ -40,26 +37,22 @@ function game() {
     playerSelection =
       playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-    // run function to determine outcome
     const outcome = playRound(playerSelection, computerSelection, round);
 
     // if a tie, round is repeated 
-    // if user wins the round, "win" is incremented
     if (outcome === "tie") {
       round--;
       console.log("You tied! Go again!");
     } else {
-      win += outcome;
+      roundsWon += outcome;
     }
 
-    // ends loop if either the computer or user has the necessary 3 wins
-    if (win === 3 || win === round - 3) break;
+    if (roundsWon === 3 || roundsWon === round - 3) break;
   }
 
-  return win;
+  return roundsWon;
 }
 
-// prints final message based on outcome: either a win or loss
 console.log(
   game() === 3 ? "YOU WON!!!!!" : "Awww, you lost... Better luck next time!"
 );
